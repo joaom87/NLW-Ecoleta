e9037830b92d289e46333bd8f0241a167c1d930c
joaom87 <joao.marcos.araujo19@gmail.com>
2020-06-22 11:02:50 -0300

    importacao inicial

---
+++ public/scripts/intro.js
@@ -0,0 +1,54 @@
+// document.write("Hello")
+
+//variaveis
+
+//let myvar = "Hi"
+//const myconst = "He"
+
+//document.write(myvar + myconst)
+
+// obejeto possuem propriedades e funcionalidades 
+
+//const pessoa = {
+//altura:"1,80m",
+//idade: 24,
+//solteiro: true,
+//correr(){
+//return "Run Forest"
+// }
+//}
+
+//document.write(pessoa.correr())
+
+//const colecaoDeBolinhas = ["blue", "green", 1, {name:"My Name"}]
+
+//document.write(colecaoDeBolinhas[3].name)
+
+//Funções
+
+//registrar
+//function sayMyName(name){
+ // document.write(name)
+//}
+
+//executar
+//sayMyName("João")
+//sayMyName("Karol")
+//sayMyName("Marcos")
+//sayMyName("Anna")
+
+//const notafinal = 5
+
+//if(notafinal < 5) {
+//    document.write("Reprovado")
+//   } else {
+//    document.write("Aprovado")
+//}
+
+
+//loop 
+
+//for(i=0; i<10; i++){
+//  document.write(`<p>${i}</p>`)
+//}
+
